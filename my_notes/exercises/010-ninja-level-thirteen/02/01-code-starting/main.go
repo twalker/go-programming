76926abf5f2d71a6c65292198563a649be92e56d
twalker <tim.twalker@gmail.com>
2022-12-16 21:03:35 -0800

    last notes

--- my_notes/exercises/010-ninja-level-thirteen/02/01-code-starting/main.go
+++ my_notes/exercises/010-ninja-level-thirteen/02/01-code-starting/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/GoesToEleven/go-programming/code_samples/010-ninja-level-thirteen/02/01-code-starting/quote"
-	"github.com/GoesToEleven/go-programming/code_samples/010-ninja-level-thirteen/02/01-code-starting/word"
+  "word"
+  "quote"
 )
 
 func main() {
